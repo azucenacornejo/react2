bb02798d5dbda902b378d35df5746e28664e3737
Blanca Azucena Cornejo <cornejoquispeazucena@gmail.com>
2023-10-18 18:11:53 -0500

    v05 agregue estilos y modifique app, añadi 04componet

---
+++ src/components/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCq2gGiEJcJnDYl33mXJs0gMHDJSPXX9Lc",
+  authDomain: "appreact-f8e75.firebaseapp.com",
+  projectId: "appreact-f8e75",
+  storageBucket: "appreact-f8e75.appspot.com",
+  messagingSenderId: "379149326038",
+  appId: "1:379149326038:web:bd7cbb0343f2f74295db77"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
