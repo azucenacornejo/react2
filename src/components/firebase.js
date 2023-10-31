9fbdf0707020a3c291db250d870754f27fb72830
Blanca Azucena <cornejoquispeazucena@gmail.com>
2023-10-31 18:29:28 -0500

    V07 actualizacion de app

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,7 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+
+import { getAuth} from "firebase/auth";
+import { getFirestore} from "firebase/firestore";
+import { getStorage} from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -13,5 +18,15 @@ const firebaseConfig = {
   appId: "1:379149326038:web:bd7cbb0343f2f74295db77"
 };
 
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage (app);
+
+
+
+
+
